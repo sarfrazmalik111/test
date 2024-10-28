9fc14b1223e05b707daa8ecdfc80ca5fa21c9b66
Sarfraz Malik <sarfrazmalik111@users.noreply.github.com>
2024-10-28 23:00:46 +0530

    Update Test.java
    Print message added

--- Test.java
+++ Test.java
@@ -3,7 +3,11 @@ import java.util.Arrays;
 public class Test {
 
     public static void main(String[] args) {
-        int[] array = {4, 2, 3,  7, 1, 2, 5, 3, 6};
-        Arrays.stream(array).sorted().forEach(System.out::println);
+        System.out.println("------------Java-Program-Start-------------");
+        int[] array = {4, 2, 3, 2, 1, 5};
+        System.out.println("Original Array: ");
+        Arrays.stream(array).forEach(a -> System.out.print(a +" "));
+        System.out.println("\nSorted Array: ");
+        Arrays.stream(array).sorted().forEach(a -> System.out.print(a +" "));
     }
 }
